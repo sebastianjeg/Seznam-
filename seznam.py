41cd30cd7ee68ce8f964d85a60a5237a1da06280
sebastianjeg <156411873+sebastianjeg@users.noreply.github.com>
2024-04-18 18:08:15 +0000

    hotovo

---
+++ seznam.py
@@ -0,0 +1,22 @@
+seznam = (11, 14, 17, 3.14, 5.6, 7)
+
+hodnota1 = seznam[0]
+hodnota2 = seznam[2]
+hodnota3 = seznam[5]
+
+celkovahodnota = sum(seznam)
+print("Celková hodnota seznamu je:", celkovahodnota)
+
+nejmensi = min(seznam)
+print("Nejmenší číslo je:",nejmensi)
+nejvetsi = max(seznam)
+print("Největší číslo je:",nejvetsi)
+
+novahodnota1 = (hodnota1-1)
+print("Nová hodnota prvního čísla je:",novahodnota1)
+
+novahodnota2 = (hodnota2-2)
+print("Nová hodnota prostředního čísla je:",novahodnota2)
+
+novahodnota3 = (hodnota3-3)
+print("Nová hodnota posledního čísla je:",novahodnota3)
